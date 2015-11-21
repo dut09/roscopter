21ec275f661bd9998f5d59c34b3d21974517bca5
Tao Du <dutao09@gmail.com>
2015-11-20 23:43:18 -0500

    Implement PID controllers for xyz positions, but untested!

--- nodes/taocopter.py
+++ nodes/taocopter.py
@@ -14,8 +14,11 @@ from mit_msgs.msg import MocapPosition
 from roscopter.msg import RC
 import sys, struct, time, os
 
+###############################################################################
 # Global data definition
+###############################################################################
 rate = 50
+dt = 1.0 / rate
 # The desired position of the copter, all in millimeters.
 # Positive z means higher altitude.
 x_target = 0
@@ -29,6 +32,25 @@ z_current = 0
 z_threshold = 20
 # Command at rest.
 command_rest = [1455, 1455, 1050, 1450, 1685, 1500, 1500, 1500]
+# PID parameters for roll, pitch and altitude. We leave yaw uncontrolled and
+# rely on the user to align the copter and the world coordinates. This could be
+# resolved by using axisangles from the VICON data, but we just want to keep
+# this example simple at the beginning.
+rp_p = 0.2
+rp_i = 0.1
+rp_d = 0.0
+r_integral = 0.0
+p_integral = 0.0
+z_p = 0.2
+z_i = 0.1
+z_d = 0.0
+z_integral = 0.0
+p_max = 150 # The max value for the proportional gain.
+i_max = 100 # The max value for the integral.
+
+
+def Clamp(val, lower, upper):
+  return min(max(val, lower), upper)
 
 def UpdateVicon(data):
   global x_current
@@ -46,9 +68,29 @@ def PublishCommand():
     if z_current <= z_threshold:
       command = command_rest 
     else:
-      #TODO: Based on the current VICON data, determine the signals we would
-      # like to send.
-      command = [0, 0, 0, 0, 0, 0, 0, 0];
+      # Let's implement a super simple P controller here to hold the altitude.
+      x_error = x_target - x_current
+      y_error = y_target - y_current
+      z_error = z_target - z_current
+      # The convention of our RC:
+      # channel 1: roll: 1000(left), 2000(right).
+      # channel 2: pitch: 1000(up), 2000(down).
+      # channel 3: throttle: 1000(down), 2000(up).
+      # channel 4: yaw: 1000(left), 2000(right).
+      # channel 5: flight mode: 1685(AltHold), 1815(Land).
+      command = command_rest
+      # Update roll for x hold.
+      r_integral = r_integral + x_error * dt
+      command[0] = command_rest[0] - Clamp(rp_p * x_error, -p_max, p_max) - \
+                   Clamp(rp_i * r_integral, -i_max, i_max)
+      # Update pitch for y hold.
+      p_integral = p_integral + y_error * dt
+      command[1] = command_rest[1] + Clamp(rp_p * y_error, -p_max, p_max) + \
+                   Clamp(rp_i * p_integral, -i_max, i_max)
+      # Update the throttle value for altitude hold.
+      z_integral = z_integral + z_error * dt
+      command[2] = 1450 + Clamp(z_p * z_error, -p_max, p_max) + \
+                   Clamp(z_i * z_integral, -i_max, i_max)
     pub.publish(command)
     r.sleep()
 
