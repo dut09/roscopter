425ba16e3ebd78eb4443315b8caa77eeba34600c
Tao Du <dutao09@gmail.com>
2015-11-21 23:02:09 -0500

    Nice parameters, could oscillate around the waypoint.

--- nodes/taocopter.py
+++ nodes/taocopter.py
@@ -32,6 +32,7 @@ z_current = 0
 # Store the old error to compute the derivatives
 x_error_old = 0
 y_error_old = 0
+z_error_old = 0
 # Use z_threshold to determine whether the VICON data is ready.
 z_threshold = 20
 # Command at rest.
@@ -43,21 +44,16 @@ ALTHOLD_MODE = 1685
 # rely on the user to align the copter and the world coordinates. This could be
 # resolved by using axisangles from the VICON data, but we just want to keep
 # this example simple at the beginning.
-rp_p = 0.5
+rp_p = 0.15
 rp_i = 0.0
-rp_d = 0.5
-z_p = 0.2
-z_i = 0.1
-z_d = 0.0
+rp_d = 0.05
+z_p = 0.25
+z_i = 0.0
+z_d = 0.5
 z_integral = 0.0
 p_max = 75 # The max value for the proportional gain.
-i_max = 100 # The max value for the integral gain.
-d_max = 150 # The max value for the derivative gain.
-
-channel_1 = 0
-channel_2 = 0
-channel_3 = 0
-channel_4 = 0
+i_max = 50 # The max value for the integral gain.
+d_max = 75 # The max value for the derivative gain.
 
 def Clamp(val, lower, upper):
   return min(max(val, lower), upper)
@@ -70,20 +66,11 @@ def UpdateVicon(data):
   y_current = data.translational.y
   z_current = data.translational.z
 
-def UpdateRC(data):
-  global channel_1
-  global channel_2
-  global channel_3
-  global channel_4 
-  channel_1 = data.channel[0]
-  channel_2 = data.channel[1]
-  channel_3 = data.channel[2]
-  channel_4 = data.channel[3]
-
 def PublishCommand():
   global z_integral
   global x_error_old
   global y_error_old
+  global z_error_old
   pub = rospy.Publisher('/send_rc', RC, queue_size = 50)
   r = rospy.Rate(rate)
   while not rospy.is_shutdown():
@@ -96,9 +83,11 @@ def PublishCommand():
       # Compute the derivatives.
       x_dev = (x_error - x_error_old) / dt
       y_dev = (y_error - y_error_old) / dt
+      z_dev = (z_error - z_error_old) / dt
       # Update old errors.
       x_error_old = x_error
       y_error_old = y_error
+      z_error_old = z_error
       # The convention of our RC:
       # channel 1: roll: 1000(left), 2000(right).
       # channel 2: pitch: 1000(up), 2000(down).
@@ -113,14 +102,12 @@ def PublishCommand():
         command[1] = command_rest[1] + Clamp(rp_p * y_error, -p_max, p_max) \
                      + Clamp(rp_d * y_dev, -d_max, d_max)
       # Update the throttle value for altitude hold.
-      z_integral = z_integral + z_error * dt
-      command[2] = 1450 + Clamp(z_p * z_error, -150, 150) + \
-                   Clamp(z_i * z_integral, -i_max, i_max)
+      command[2] = 1450 + Clamp(z_p * z_error, -125, 125) + \
+                   Clamp(z_d * z_dev, -d_max, d_max)
     if is_land_mode:
       command[4] = LAND_MODE
     else:
       command[4] = ALTHOLD_MODE
-    print x_current, y_current, z_current, channel_1, channel_2, channel_3, channel_4
     pub.publish(command)
     r.sleep()
 
@@ -142,7 +129,6 @@ if __name__ == '__main__':
     # ServiceL change the flight mode.
     rospy.Service('setmode', ChangeMode, SetFlightMode)
     rospy.Subscriber("/TaoCopter", MocapPosition, UpdateVicon)
-    rospy.Subscriber("/rc", RC, UpdateRC)
     PublishCommand()
     rospy.spin()
   except rospy.ROSInterruptException:
